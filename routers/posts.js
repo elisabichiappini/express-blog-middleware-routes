a0e1ceba2b0fd8179a608147b336cfbfd956e049
Elisa Chiappini <elisabichiappini@gmail.com>
2025-02-18 23:06:55 +0100

    feat: mettere lo use middleware in router

--- routers/posts.js
+++ routers/posts.js
@@ -4,6 +4,9 @@ const router = express.Router();
 //importiamo qui il controller perchè ci saranno due rotte con "/" in get e post
 const postsController = require("../controllers/posts.js");
 
+//settiamo il bodyparser, con extended true per leggere array e oggetti se no li legge come stringhe
+router.use(express.urlencoded({extended: true}));
+
 //rotta /posts
 router.get("/", postsController.show);
 router.post("/", postsController.store);
