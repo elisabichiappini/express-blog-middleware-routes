c2a93314c0aaf012fc38964e52ce30e5cd1eff52
Elisa Chiappini <elisabichiappini@gmail.com>
2025-02-18 22:41:53 +0100

    feat: posts controllers

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,7 +7,7 @@ const show = (req, res) => {
             posts.forEach(post => {
                 html += `
             <li>
-                <h3>titolo: ${post.title}</h3>
+                <h3>${post.title}</h3>
                 <p>${post.content}</p>
                 <img width="200px" src=${`/${post.image}`}/>
                 <h6>tags: ${post.tags}</h6>
@@ -15,12 +15,11 @@ const show = (req, res) => {
                 <a href="/posts/${post.slug}/download" target="_blank"> Scarica immagine </a>
             </li>`;
         });
-                html += '</ul>';
-                res.send(html);
-
+            html += '</ul>';
+            res.send(html);
         },
     })
-}
+};
 
 module.exports = {
     show
