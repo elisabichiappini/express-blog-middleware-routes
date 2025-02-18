739e0292480c1bc6557461c3052c283348db5556
Elisa Chiappini <elisabichiappini@gmail.com>
2025-02-18 23:04:55 +0100

    feat: add urlencoded per il parser del body

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //configurazione dotenv
 const dotenv = require("dotenv");
 dotenv.config();
-
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const postsRouter = require("./routers/posts.js");
+
+//settiamo il bodyparser, con extended true per leggere array e oggetti se no li legge come stringhe
+app.use(express.urlencoded({extended: true}))
 
 //middleware per public
 app.use(express.static("./public"));
