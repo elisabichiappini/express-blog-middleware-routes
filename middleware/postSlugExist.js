700af6f415b431b6cc83dba9e67fb599dce28460
Elisa Chiappini <elisabichiappini@gmail.com>
2025-02-19 23:11:27 +0100

    feat: middleware del post slug exist completo

--- middleware/postSlugExist.js
+++ middleware/postSlugExist.js
@@ -5,8 +5,9 @@ module.exports = (req, res, next) => {
     const { slug } = req.params;
     const sluggedPost = posts.find(p => p.slug === slug);
 
-    if(sluggedPost) {
-        //scriverlo subito per chè va eseguito
-        next();
+    if(!sluggedPost) {
+        return res.status(404).send(`post con slug ${slug} non trovato`)
     }
+    //scriverlo subito per chè va eseguito
+    next();
 }
